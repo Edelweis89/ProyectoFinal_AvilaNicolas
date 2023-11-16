c9fcc3382df34753172d1014e3cc6174d7e865bf
Nicolas Avila <nicolas.avila09@outlook.com>
2023-11-15 23:48:28 -0300

    conectando api por primera ves

--- javascript/buscador.js
+++ javascript/buscador.js
@@ -1,55 +1,57 @@
-///traemos el elemento form para trabajar con el y le asignamos una funcion al hacer submit en el mismo.
+const filtroPj = "https://rickandmortyapi.com/api/character";
+console.log(filtroPj);
+//traemos el elemento form para trabajar con el y le asignamos una funcion al hacer submit en el mismo.
 
-document.getElementById("buscarForm").addEventListener("submit", (e) => {
-  e.preventDefault();
+// document.getElementById("buscarForm").addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  // Si hay informacion ya en el elemento lo borramos para una nueva busqueda sino realiza busqueda.
-  document.getElementById("buscadorTarjetas").innerHTML = "";
+//   // Si hay informacion ya en el elemento lo borramos para una nueva busqueda sino realiza busqueda.
+//   document.getElementById("buscadorTarjetas").innerHTML = "";
 
-  /// traemos el valor del input.
-  let busqueda = document.getElementById("buscarBtn").value;
+//   /// traemos el valor del input.
+//   let busqueda = document.getElementById("buscarBtn").value;
 
-  /// almacenamos en localstorage el valor.
-  localStorage.setItem("busqueda", busqueda);
+//   /// almacenamos en localstorage el valor.
+//   localStorage.setItem("busqueda", busqueda);
 
-  /// buscamos en el objeto data la informacion y si es igual traemos el contenido acorde.
-  let buscarName = data.find(
-    (name) => name.name === localStorage.getItem("busqueda")
-  );
+//   /// buscamos en el objeto data la informacion y si es igual traemos el contenido acorde.
+//   let buscarName = results.find(
+//     (name) => name.name === localStorage.getItem("busqueda")
+//   );
 
-  /// ejecutamos funcion para mostrar info en el dom de nuestro html.
-  buscarName
-    ? crearNodeBuscador(buscarName, document.getElementById("buscadorTarjetas"))
-    : Swal.fire("Personaje no encontrado, intenta nuevamente!");
+//   /// ejecutamos funcion para mostrar info en el dom de nuestro html.
+//   buscarName
+//     ? crearNodeBuscador(buscarName, document.getElementById("buscadorTarjetas"))
+//     : Swal.fire("Personaje no encontrado, intenta nuevamente!");
 
-  function crearNodeBuscador(array, container) {
-    const cardBuscador = document.createElement("div");
-    cardBuscador.className = "cardBuscador";
-    cardBuscador.innerHTML = `
-         <h3 class= "tituloCardBuscador"> ${array.name} </h3>
-         <div class= "contenidoCardBuscador">
-             <p> Status: ${array.status} </p>
-             <p> Species: ${array.species} </p>
-             <p> Gender: ${array.gender} </p>
-             <p> Origin: ${array.origin.name} </p>
-             <p> Location: ${array.location.name} </p>
-             <p> URL: <a>${array.location.url}</a> </p>
-             <p> Created: ${array.created}</p>
-     
-         </div>
-         <div class= imgCardBuscador>
-             <img src="${array.image}" alt= ${array.name}>
-         </div>
-         <button id="agregarFavoritos">
-           Agregar a Favoritos
-         </button>
-         `;
-    container.appendChild(cardBuscador);
-  }
-});
+//   function crearNodeBuscador(array, container) {
+//     const cardBuscador = document.createElement("div");
+//     cardBuscador.className = "cardBuscador";
+//     cardBuscador.innerHTML = `
+//          <h3 class= "tituloCardBuscador"> ${array.name} </h3>
+//          <div class= "contenidoCardBuscador">
+//              <p> Status: ${array.status} </p>
+//              <p> Species: ${array.species} </p>
+//              <p> Gender: ${array.gender} </p>
+//              <p> Origin: ${array.origin.name} </p>
+//              <p> Location: ${array.location.name} </p>
+//              <p> URL: <a>${array.location.url}</a> </p>
+//              <p> Created: ${array.created}</p>
 
-setTimeout(() => {
-  Swal.fire(
-    "Busca informacion de los Personajes de la Serie! Puedes Agregar el que quieras a tus Favoritos"
-  );
-}, 1000);
+//          </div>
+//          <div class= imgCardBuscador>
+//              <img src="${array.image}" alt= ${array.name}>
+//          </div>
+//          <button id="agregarFavoritos">
+//            Agregar a Favoritos
+//          </button>
+//          `;
+//     container.appendChild(cardBuscador);
+//   }
+// });
+
+// setTimeout(() => {
+//   Swal.fire(
+//     "Busca informacion de los Personajes de la Serie! Puedes Agregar el que quieras a tus Favoritos"
+//   );
+// }, 1000);
--- javascript/mostrarcharacters.js
+++ javascript/mostrarcharacters.js
@@ -1,29 +1,43 @@
-crearNode(data, document.getElementById("cardsContainer"));
+const containerCards = document.querySelector("#cardsContainer");
 
-function crearNode(array, container) {
-  for (let i = 0; i < array.length; i++) {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-     <h3 class= "tituloCard"> ${array[i].name} </h3>
-     <div class= "contenidoCard"> 
-         <p> Status: ${array[i].status} </p>
-         <p> Species: ${array[i].species} </p>
-         <p> Gender: ${array[i].gender} </p>
-         <p> Origin: ${array[i].origin.name} </p>
-         <p> Location: ${array[i].location.name} </p>
+const nodosPersonajes = (results, container) => {
+  const nodos = results.reduce((acc, element) => {
+    return (
+      acc +
+      `
+    <article class="card"> 
+    <h3 class= "tituloCard">${element.name}</h3>
+    <div class= "contenidoCard">
+    <p> Status: ${element.status} </p>
+          <p> Species: ${element.species} </p>
+          <p> Gender: ${element.gender} </p>
+          <p> Origin: ${element.origin.name} </p>
+          <p> Location: ${element.location.name}</p>
+          <p> Created: ${element.created}</p>
  
-     </div>
-     <div class= imgCard> 
-         <img src="${array[i].image}" alt= ${array[i].name}>
-     </div>
-    
-     `;
-    container.appendChild(card);
-    // container.appendChild(card);
-  }
-}
+          </div>
+    <div class= imgCard>
+          <img src="${element.image}" alt= ${element.name}>
+      </div>
+  
+</article>
+    `
+    );
+  }, "");
 
-setTimeout(() => {
-  Swal.fire("Bienvenido al Sitio de Rick & Morty!");
-}, 1000);
+  container.innerHTML = nodos;
+};
+
+const llamadoApi = (url, generarNodos, container) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((results) => {
+      generarNodos(results, container);
+    });
+};
+
+llamadoApi(
+  "https://rickandmortyapi.com/api/character/1, 2, 3, 4, 5, 6, 7, 8, 34, 56, 67, 42, 59, 24, 22, 116, 180, 146, 148, 183",
+  nodosPersonajes,
+  containerCards
+);
