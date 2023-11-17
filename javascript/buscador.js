ef24b7cad03b407f51c3f722e08f964202478b0b
Nicolas Avila <nicolas.avila09@outlook.com>
2023-11-16 23:51:57 -0300

    index y buscador js funcionando desde api ok

--- javascript/buscador.js
+++ javascript/buscador.js
@@ -1,57 +1,97 @@
-const filtroPj = "https://rickandmortyapi.com/api/character";
-console.log(filtroPj);
-//traemos el elemento form para trabajar con el y le asignamos una funcion al hacer submit en el mismo.
+let personajesFiltro;
+let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
 
-// document.getElementById("buscarForm").addEventListener("submit", (e) => {
-//   e.preventDefault();
+const subirAlLS = (clave, data) =>
+  localStorage.setItem(clave, JSON.stringify(data));
+const obtenerDelLs = (clave) => JSON.parse(localStorage.getItem(clave));
+const agregarFavorito = (data) => favoritos.push(data);
+const obtenerPj = (clase, data) => {
+  const obtenerId = clase.slice(4);
+  return data.find((element) => element.id === Number(obtenerId));
+};
 
-//   // Si hay informacion ya en el elemento lo borramos para una nueva busqueda sino realiza busqueda.
-//   document.getElementById("buscadorTarjetas").innerHTML = "";
+function limpiarBusqueda() {
+  document.getElementById("buscadorTarjetas").innerHTML = "";
+}
 
-//   /// traemos el valor del input.
-//   let busqueda = document.getElementById("buscarBtn").value;
+const nodosPersonajesBuscador = (results, container) => {
+  const nodoBuscador = results.reduce((acc, element) => {
+    return (
+      acc +
+      `
+      
+      <article class="cardBuscador"> 
+      <h3 class= "tituloCardBuscador">${element.name}</h3>
+      <div class= "contenidoCardBuscador">
+      <p> Status: ${element.status} </p>
+            <p> Species: ${element.species} </p>
+            <p> Gender: ${element.gender} </p>
+            <p> Origin: ${element.origin.name} </p>
+            <p> Location: ${element.location.name}</p>
+            <p> Created: ${element.created}</p>
+   
+            </div>
+      <div class= imgCardBuscador>
+            <img src="${element.image}" alt= ${element.name}>
+        </div>
+        <button class="addfav" id="add-${element.id}">
+        <i class="addfav fa-solid fa-star id="add-${element.id} style="color: #982ddf;"></i>
+                    
+                </button>
+    
+  </article>
+      `
+    );
+  }, "");
 
-//   /// almacenamos en localstorage el valor.
-//   localStorage.setItem("busqueda", busqueda);
+  container.innerHTML = nodoBuscador;
+};
 
-//   /// buscamos en el objeto data la informacion y si es igual traemos el contenido acorde.
-//   let buscarName = results.find(
-//     (name) => name.name === localStorage.getItem("busqueda")
-//   );
+const llamadoApiBuscador = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((results) => {
+      personajesFiltro = results;
+    });
+};
 
-//   /// ejecutamos funcion para mostrar info en el dom de nuestro html.
-//   buscarName
-//     ? crearNodeBuscador(buscarName, document.getElementById("buscadorTarjetas"))
-//     : Swal.fire("Personaje no encontrado, intenta nuevamente!");
+llamadoApiBuscador(
+  "https://rickandmortyapi.com/api/character/1, 2, 3, 4, 5, 6, 7, 8, 34, 56, 67, 42, 59, 24, 22, 116, 180, 146, 148, 183"
+);
 
-//   function crearNodeBuscador(array, container) {
-//     const cardBuscador = document.createElement("div");
-//     cardBuscador.className = "cardBuscador";
-//     cardBuscador.innerHTML = `
-//          <h3 class= "tituloCardBuscador"> ${array.name} </h3>
-//          <div class= "contenidoCardBuscador">
-//              <p> Status: ${array.status} </p>
-//              <p> Species: ${array.species} </p>
-//              <p> Gender: ${array.gender} </p>
-//              <p> Origin: ${array.origin.name} </p>
-//              <p> Location: ${array.location.name} </p>
-//              <p> URL: <a>${array.location.url}</a> </p>
-//              <p> Created: ${array.created}</p>
+// filtrar por búsqueda
+const inputBusqueda = document.querySelector("#input-busqueda");
 
-//          </div>
-//          <div class= imgCardBuscador>
-//              <img src="${array.image}" alt= ${array.name}>
-//          </div>
-//          <button id="agregarFavoritos">
-//            Agregar a Favoritos
-//          </button>
-//          `;
-//     container.appendChild(cardBuscador);
-//   }
-// });
+document.querySelector("#buscarForm").onsubmit = (event) => {
+  event.preventDefault();
+  const filteredCharacters = personajesFiltro.filter((personaje) =>
+    personaje.name.toLowerCase().includes(inputBusqueda.value.toLowerCase())
+  );
+  nodosPersonajesBuscador(filteredCharacters, buscadorTarjetas);
+};
 
-// setTimeout(() => {
-//   Swal.fire(
-//     "Busca informacion de los Personajes de la Serie! Puedes Agregar el que quieras a tus Favoritos"
-//   );
-// }, 1000);
+// agregar a favoritos a partir del evento.
+document.body.onclick = (event) => {
+  if (event.target.classList.contains("addfav")) {
+    agregarFavorito(obtenerPj(event.target.id, personajesFiltro));
+    subirAlLS("favoritos", favoritos);
+    Toastify({
+      text: `El personaje ${
+        obtenerPj(event.target.id, personajesFiltro).name
+      } ha sido añadido a Favoritos.`,
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+      onClick: function () {},
+    }).showToast();
+  }
+};
+
+/// en este punto agrego favoritos al local storage, si no tiene la clase addfav agrega null al ls si la tiene agrega el favorito ok. ver como solucionar eso.
