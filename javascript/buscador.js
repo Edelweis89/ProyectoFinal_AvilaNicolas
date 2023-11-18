fcbe27a65a73e8d3747b4296fe6b931523f539de
Nicolas Avila <nicolas.avila09@outlook.com>
2023-11-18 05:05:32 -0300

    ultimando detalles antes de la entrega final

--- javascript/buscador.js
+++ javascript/buscador.js
@@ -1,26 +1,36 @@
 let personajesFiltro;
 let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
 
-const subirAlLS = (clave, data) =>
-  localStorage.setItem(clave, JSON.stringify(data));
-const obtenerDelLs = (clave) => JSON.parse(localStorage.getItem(clave));
-const agregarFavorito = (data) => favoritos.push(data);
+function agregarFavorito(personaje) {
+  favoritos.push(personaje);
+}
+
+function eliminarFavorito(personaje) {
+  const index = favoritos.indexOf(personaje);
+
+  if (index !== -1) {
+    favoritos.splice(index, 1);
+  }
+}
+
+function actualizarLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
 const obtenerPj = (clase, data) => {
   const obtenerId = clase.slice(4);
   return data.find((element) => element.id === Number(obtenerId));
 };
 
-function limpiarBusqueda() {
-  document.getElementById("buscadorTarjetas").innerHTML = "";
-}
+/// crear tarjetas con Doom para mostrar la info de los personajes Buscados.
 
 const nodosPersonajesBuscador = (results, container) => {
   const nodoBuscador = results.reduce((acc, element) => {
     return (
       acc +
       `
-      
-      <article class="cardBuscador"> 
+
+      <article class="cardBuscador">
       <h3 class= "tituloCardBuscador">${element.name}</h3>
       <div class= "contenidoCardBuscador">
       <p> Status: ${element.status} </p>
@@ -29,16 +39,21 @@ const nodosPersonajesBuscador = (results, container) => {
             <p> Origin: ${element.origin.name} </p>
             <p> Location: ${element.location.name}</p>
             <p> Created: ${element.created}</p>
-   
+
             </div>
       <div class= imgCardBuscador>
             <img src="${element.image}" alt= ${element.name}>
         </div>
-        <button class="addfav" id="add-${element.id}">
-        <i class="addfav fa-solid fa-star id="add-${element.id} style="color: #982ddf;"></i>
-                    
+        <button class="addfav" id="add-${element.id}"> Agregar a Favoritos
+        
+
                 </button>
-    
+                <button class="removeFav" id="add-${element.id}">
+                Eliminar de Favoritos
+        
+
+                </button>
+
   </article>
       `
     );
@@ -47,6 +62,7 @@ const nodosPersonajesBuscador = (results, container) => {
   container.innerHTML = nodoBuscador;
 };
 
+/// llamado a la api
 const llamadoApiBuscador = (url) => {
   fetch(url)
     .then((res) => res.json())
@@ -59,7 +75,7 @@ llamadoApiBuscador(
   "https://rickandmortyapi.com/api/character/1, 2, 3, 4, 5, 6, 7, 8, 34, 56, 67, 42, 59, 24, 22, 116, 180, 146, 148, 183"
 );
 
-// filtrar por búsqueda
+// filtrar por búsqueda de nombre
 const inputBusqueda = document.querySelector("#input-busqueda");
 
 document.querySelector("#buscarForm").onsubmit = (event) => {
@@ -70,11 +86,12 @@ document.querySelector("#buscarForm").onsubmit = (event) => {
   nodosPersonajesBuscador(filteredCharacters, buscadorTarjetas);
 };
 
-// agregar a favoritos a partir del evento.
+/// Proceso de agregar o eliminar favoritos segun el evento escuchado en el body por el navegador. Si el evento contiene la clase addfav se agregar la tarjeta a favorita, si el evento contiene la clase removeFav, la tarjeta se elimina de favoritos.
+
 document.body.onclick = (event) => {
   if (event.target.classList.contains("addfav")) {
     agregarFavorito(obtenerPj(event.target.id, personajesFiltro));
-    subirAlLS("favoritos", favoritos);
+    actualizarLocalStorage("favoritos", favoritos);
     Toastify({
       text: `El personaje ${
         obtenerPj(event.target.id, personajesFiltro).name
@@ -92,6 +109,21 @@ document.body.onclick = (event) => {
       onClick: function () {},
     }).showToast();
   }
-};
 
-/// en este punto agrego favoritos al local storage, si no tiene la clase addfav agrega null al ls si la tiene agrega el favorito ok. ver como solucionar eso.
+  if (event.target.classList.contains("removeFav")) {
+    const personaje = obtenerPj(event.target.id, favoritos);
+    eliminarFavorito(personaje);
+    actualizarLocalStorage("favoritos", favoritos);
+
+    Toastify({
+      text: `El personaje ${personaje.name} ha sido eliminado de Favoritos.`,
+      duration: 3000,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #c0392b, #8e44ad)",
+      },
+    }).showToast();
+  }
+};
---
+++ javascript/favoritos.js
@@ -0,0 +1,131 @@
+const obtenerlistaFavoritos = JSON.parse(localStorage.getItem("favoritos"));
+
+const containerCardsFavoritos = document.querySelector(
+  "#cardsContainerFavoritos"
+);
+
+const obtenerPjFav = (clase, data) => {
+  const obtenerId = clase.slice(4);
+  return data.find((element) => element.id === Number(obtenerId));
+};
+
+function eliminarFavorito(personaje) {
+  const index = obtenerlistaFavoritos.indexOf(personaje);
+
+  if (index !== -1) {
+    obtenerlistaFavoritos.splice(index, 1);
+  }
+}
+
+function actualizarLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+const nodosPersonajesFav = (results, container) => {
+  const nodos = results.reduce((acc, element) => {
+    return (
+      acc +
+      `
+    <article class="cardFavoritos"> 
+    <h3 class= "tituloCardFavoritos">${element.name}</h3>
+    <div class= "contenidoCardFavoritos">
+    <p> Status: ${element.status} </p>
+          <p> Species: ${element.species} </p>
+          <p> Gender: ${element.gender} </p>
+          <p> Origin: ${element.origin.name} </p>
+          <p> Location: ${element.location.name}</p>
+          <p> Created: ${element.created}</p>
+ 
+          </div>
+    <div class= imgCardFavoritos>
+          <img src="${element.image}" alt= ${element.name}>
+      </div>
+      
+      <button class="removeFav" id="add-${element.id}">
+      Eliminar de Favoritos
+      </button>
+</article>
+    `
+    );
+  }, "");
+
+  container.innerHTML = nodos;
+};
+
+nodosPersonajesFav(obtenerlistaFavoritos, cardsContainerFavoritos);
+
+// Función para eliminar todos los elementos de un array almacenado en local storage
+function eliminarTodosLosFavoritos() {
+  obtenerlistaFavoritos.length = 0;
+}
+
+// Función para eliminar todas las tarjetas de favoritos
+function eliminarTodasLasTarjetasFavoritos() {
+  let tarjetasFavoritos = document.querySelectorAll(".cardFavoritos");
+  tarjetasFavoritos.forEach((tarjeta) => tarjeta.remove());
+}
+
+// En el siguiente evento click, capturo por medio del atributo target la clase donde fue escuchado el evento, segun la clase aplico las funciones que corresponden a cada una y de paso controlo el comportamiento de cada evento.
+
+document.body.onclick = (event) => {
+  if (event.target.classList.contains("orderAsc")) {
+    /// Orden Ascendente de las Tarjetas...(funcion sort() con los atributos name.)
+    const ordenarTarjetas = obtenerlistaFavoritos.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    eliminarTodasLasTarjetasFavoritos();
+    nodosPersonajesFav(ordenarTarjetas, cardsContainerFavoritos);
+    actualizarLocalStorage("favoritos", ordenarTarjetas);
+  } else if (event.target.classList.contains("orderDes")) {
+    /// Orden Desendente de las Tarjetas...(funcion sort() con los atributos name.)
+    const ordenarTarjetas = obtenerlistaFavoritos.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return -1;
+      }
+      return 0;
+    });
+    eliminarTodasLasTarjetasFavoritos();
+    nodosPersonajesFav(ordenarTarjetas, cardsContainerFavoritos);
+    actualizarLocalStorage("favoritos", ordenarTarjetas);
+  } else if (event.target.classList.contains("eliminarGlobalFav")) {
+    /// Elimina Todo el contenido del Local Storage y se limpia la lista Favoritos.
+    eliminarTodosLosFavoritos();
+    actualizarLocalStorage("favoritos", obtenerlistaFavoritos);
+    eliminarTodasLasTarjetasFavoritos();
+    Toastify({
+      text: `Se han eliminado todos los favoritos.`,
+      duration: 3000,
+      gravity: "bottom",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #ff576d ,#1a1aff)",
+      },
+    }).showToast();
+  } else if (event.target.classList.contains("removeFav")) {
+    //Eliminar un solo personaje a traves de su id desde el boton de la tarjeta.
+    const personajeFav = obtenerPjFav(event.target.id, obtenerlistaFavoritos);
+    eliminarFavorito(personajeFav);
+    actualizarLocalStorage("favoritos", obtenerlistaFavoritos);
+    event.target.parentElement.remove();
+    Toastify({
+      text: `El personaje ${personajeFav.name} ha sido eliminado de Favoritos.`,
+      duration: 3000,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #c0392b, #8e44ad)",
+      },
+    }).showToast();
+  }
+};
